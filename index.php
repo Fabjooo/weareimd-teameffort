d527dc048be44f3edf7e6a0e8baa0757b15d87eb
Joris Hens <joris@goodbytes.be>
2015-01-05 11:36:57 +0100

    blocked another user
    Signed-off-by: Joris Hens <joris@goodbytes.be>

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		CURLOPT_SSL_VERIFYPEER		disable the SSL certificate check (not recommended)
 	*/
 	
-	$ignore_users = array('tomylin_sk', 'live_free_tally');
+	$ignore_users = array('tomylin_sk', 'live_free_tally', 'imtoofabluv');
 
     $request = curl_init(); 								// initialize a new CURL request
     curl_setopt($request, CURLOPT_URL, $url); 				// set the url to call
