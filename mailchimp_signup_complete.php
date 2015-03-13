29e4243176307906cdfa4144ae73aaf605db088e
Joris Hens <joris@goodbytes.be>
2015-03-13 20:43:04 +0100

    Added mailchimp signup complete page
    Signed-off-by: Joris Hens <joris@goodbytes.be>

---
+++ mailchimp_signup_complete.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Interactive Multimedia Design - Thomas More Mechelen</title>
+	<link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
+	<link href='http://fonts.googleapis.com/css?family=Vollkorn:400italic,400' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Voces' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css">
+	<meta name="viewport" content="width=device-width">
+</head>
+<body class"Mailchimp-Success">
+	<header>
+		<h1>Boo-yah!</h1>
+		<img src="img/booyah.gif" alt="">
+		<p>Je eerste stappen om een echte <em>IMD'er</em> te worden zijn gezet. De komende weken zullen we je enkele mailtjes sturen met tips van onze studenten en inside information over je nieuwe opleiding!</p>
+
+		<p>Proficiat met je studiekeuze alvast! <a href="https://www.facebook.com/WeAreIMD">#WeAreIMD</a>, en jij binnenkort ook!</p>
+	
+
+	<div class="facebook">
+		<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=528896287219721";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+		<div class="fb-like" data-href="http://www.facebook.com/weareimd" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+	</div>
+
+	</header>
+
+	
+</body>
+</html>
